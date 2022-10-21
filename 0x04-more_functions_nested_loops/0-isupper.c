27e9fb9c04c33fdeff626beefe158699588819bf
Raphael_Kumako <raph.kumako@gmail.com>
2022-10-21 16:05:33 -0700

    betty is fine

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,28 +1,11 @@
-/*
- *
- *  * File: 0-isupper.c
- *
- *   * Auth: Suara Ayomide
- *
- *    */
-
-
-
 #include "main.h"
 
-
-
 /**
+ *_isupper - Checks for uppercase characters.
+ @c: The character to be checked.
+ *Return: 1 if character is uppercase, 0 otherwise.
  *
- *  * _isupper - Checks for uppercase characters.
- *
- *   * @c: The character to be checked.
- *
- *    *
- *
- *     * Return: 1 if character is uppercase, 0 otherwise.
- *
- *      */
+ */
 
 int _isupper(int c)
 
